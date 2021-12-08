645032fec29f372f3fa3e96cf084dc58b0627c7d
Achilles8055 <josejimmy8055@gmail.com>
2021-12-08 11:20:57 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,14 @@
+function buttonclick(val)
+{
+    document.getElementById("screen").value+=val
+}
+function clearDisplay()
+{
+    document.getElementById("screen").value=""
+}
+function equalClick()
+{
+    var text=document.getElementById("screen").value
+    var result=eval(text)
+    document.getElementById("screen").value=result
+}
